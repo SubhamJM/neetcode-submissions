a28393488fd9f56548cfc4096bce76380c83b3bf
Subhamjyoti Maharana <subhamjyotimaharana@gmail.com>
2026-04-30 13:04:17 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cpp
@@ -0,0 +1,52 @@
+#include <string>
+
+struct trnode {
+    trnode* ptr[26];
+    bool eow = false;
+
+    // Initialize all children to nullptr
+    trnode() {
+        for (int i = 0; i < 26; ++i) ptr[i] = nullptr;
+    }
+};
+
+class PrefixTree {
+public:
+    trnode* root;
+
+    PrefixTree() {
+        root = new trnode();
+    }
+
+    void insert(const std::string& word) {
+        trnode* curr = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (!curr->ptr[index]) {
+                curr->ptr[index] = new trnode();
+            }
+            curr = curr->ptr[index];
+        }
+        curr->eow = true;
+    }
+
+    bool search(const std::string& word) {
+        trnode* curr = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (!curr->ptr[index]) return false;
+            curr = curr->ptr[index];
+        }
+        return curr->eow;
+    }
+
+    bool startsWith(const std::string& prefix) {
+        trnode* curr = root;
+        for (char ch : prefix) {
+            int index = ch - 'a';
+            if (!curr->ptr[index]) return false;
+            curr = curr->ptr[index];
+        }
+        return true;
+    }
+};
